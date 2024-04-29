37d1ac20eb27336b35230b4f6c86838a0eb41795
Vedant Agrawal <vrsagrawal@gmail.com>
2024-04-30 01:42:12 +0530

    Added Probability Score Feature

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers import pipeline   
 import torch
 import nltk
 from nltk.util import ngrams
@@ -15,6 +16,7 @@ nltk.download('stopwords')
 # Load GPT-2 tokenizer and model
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2LMHeadModel.from_pretrained('gpt2')
+pipe = pipeline("text-classification", model="textattack/bert-base-uncased-SST-2")
 
 def calculate_perplexity(text):
     encoded_input = tokenizer.encode(text, add_special_tokens=False, return_tensors='pt')
@@ -54,6 +56,20 @@ def plot_top_repeated_words(text):
     fig = px.bar(x=words, y=counts, labels={'x': 'Words', 'y': 'Counts'}, title='Top 10 Most Repeated Words')
     st.plotly_chart(fig, use_container_width=True)
 
+def probability_score(text):
+    scores = pipe(text)
+    output = scores[0]
+    output["score"] = int(100 * output["score"])
+    if output["label"] == "LABEL_0":
+        output["label"] = "Human written"
+        output["human_score"] = output["score"]
+        output["ai_score"] = 100 - output["score"]
+    else:
+        output["label"] = "AI-generated"
+        output["ai_score"] = output["score"]
+        output["human_score"] = 100 - output["score"]
+    return output
+
 st.set_page_config(layout="wide")
 
 st.title("TextTrace: Your AI Detector🔍")
@@ -74,11 +90,14 @@ if text_area is not None:
             st.write("Perplexity:", perplexity)
             st.write("Burstiness Score:", burstiness_score)
 
-            if perplexity > 25000 and burstiness_score < 0.2:
+            score = probability_score(text_area)
+            st.write(f'AI-generated: {score["ai_score"]}%')
+            st.write(f'Human-written: {score["human_score"]}%')
+
+            if perplexity > 25000 and burstiness_score < 0.3:
                 st.error("Text Analysis Result: AI generated content")
             else:
-                st.success("Text Analysis Result: Likely Human generated content")
-            
+                st.success("Text Analysis Result: Likely Human written content")
         
             
             
